6ef02d05ae5663919df2b90c96198770b9e81380
shiny-pancake <264254580+shiny-pancake@users.noreply.github.com>
2026-02-28 08:53:38 +0000

    feat: implement rounds, trades, and bundles

---
+++ src/services/simulation.js
@@ -0,0 +1,38 @@
+class Round {
+  constructor(trades) {
+    this.trades = trades;
+  }
+
+  addTrade(node_one, node_two) {
+    const newTrade = new Trade(node_one, node_two, this);
+    this.trades.push(newTrade);
+  }
+
+  getTrades() {
+    return this.trades;
+  }
+}
+
+class Trade {
+  constructor(nodeOne, nodeTwo, bundle, round, toNodeTwo = true) {
+    console.assert(
+      nodeOne !== nodeTwo,
+      'A trade cannot be between the same node',
+    );
+    console.assert(
+      round instanceof Round,
+      'The round must be an instance of the Round class',
+    );
+
+    this.node_one = nodeOne ? toNodeTwo : nodeTwo;
+    this.node_two = nodeTwo ? toNodeTwo : nodeOne;
+    this.parentRound = round;
+    this.bundle = bundle;
+  }
+}
+
+class Bundle {
+  constructor(goodsNumber) {
+    this.goodsNumber = goodsNumber;
+  }
+}
