fa09feb878fdb8c4ed6982ab5eae126331440479
shiny-pancake <264254580+shiny-pancake@users.noreply.github.com>
2026-02-28 08:55:05 +0000

    feat: add trustGraph class for managing trust

---
+++ src/services/trust.js
@@ -0,0 +1,23 @@
+import { Graph } from "graph-data-structure";
+
+class trustGraph {
+  constructor(nodes) {
+    this.graph = new Graph();
+    this.nodes = nodes;
+    this.pairs = this.generatePairs(this.nodes);
+
+    this.addNodes()
+  }
+
+  *addNodes() {
+    this.pairs.forEach((a, b) => {
+      this.graph.addEdge(a, b);
+    });
+  }
+
+  *generatePairs() {
+    return this.nodes.flatMap(
+      (v, i) => this.nodes.slice(i+1).map( w => (v, w))
+    );
+  }
+}
